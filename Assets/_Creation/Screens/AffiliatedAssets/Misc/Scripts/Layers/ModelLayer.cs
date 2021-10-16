606d52df0f09b452a142644ed26842eedb7bdb58
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2021-10-16 23:32:28 +0800

    Layers

---
+++ Assets/_Creation/Screens/AffiliatedAssets/Misc/Scripts/Layers/ModelLayer.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+namespace Genesis.Creation {
+    internal sealed class ModelLayer: MonoBehaviour {
+    }
+}
---
+++ Assets/_Creation/Screens/AffiliatedAssets/Misc/Scripts/Layers/ViewLayer.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+namespace Genesis.Creation {
+    internal sealed class ViewLayer: MonoBehaviour {
+    }
+}
