dc2fb9d968efeac80926006916dc3e47ff8096e8
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2021-10-16 21:19:15 +0800

    DuplicateDestroyer.cs (better)

--- Assets/_Wisdom/Main/Utility/PlayMode/DuplicateDestroyer/DuplicateDestroyer.cs
+++ Assets/_Wisdom/Main/Utility/PlayMode/DuplicateDestroyer/DuplicateDestroyer.cs
@@ -1,28 +1,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Genesis.Wisdom {
 	internal sealed class DuplicateDestroyer: MonoBehaviour {
 		[SerializeField]
 		private Component[] componentArr;
 
-		private void Start() {
-			List<Object> objList;
+		private void Awake() {
+			SceneManager.sceneLoaded += (scene, loadSceneMode) => { //hmmmm
+				List<Object> objList;
 
-			foreach(Component component in componentArr) {
-				objList = FindObjectsOfType(component.GetType()).ToList();
+				foreach(Component component in componentArr) {
+					objList = FindObjectsOfType(component.GetType()).ToList();
 
-				if(objList.Count > 1) {
-					if(!objList.Remove(component)) {
-						UnityEngine.Assertions.Assert.IsTrue(false, "!objList.Remove(component)");
-					}
+					if(objList.Count > 1) {
+						if(!objList.Remove(component)) {
+							UnityEngine.Assertions.Assert.IsTrue(false, "!objList.Remove(component)");
+						}
 
-					foreach(Object obj in objList) {
-						Destroy(obj);
+						foreach(Object obj in objList) {
+							Destroy(obj);
+						}
 					}
 				}
-			}
+			};
 		}
 	}
 }
