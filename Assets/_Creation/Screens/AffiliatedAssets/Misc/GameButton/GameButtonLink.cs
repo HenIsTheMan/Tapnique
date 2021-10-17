1b936fb7dfb585136d2f02cd152f7cc054f08960
HenIsTheMan <193541T@mymail.nyp.edu.sg>
2021-10-17 22:14:49 +0800

    GameButtonLink.cs (better)

--- Assets/_Creation/Screens/AffiliatedAssets/Misc/GameButton/GameButtonLink.cs
+++ Assets/_Creation/Screens/AffiliatedAssets/Misc/GameButton/GameButtonLink.cs
@@ -1,9 +1,22 @@
 using Genesis.Wisdom;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace Genesis.Creation {
     internal sealed class GameButtonLink: MonoBehaviour { //POD
+        [field: SerializeField]
+        internal Image Img {
+            get;
+            private set;
+        }
+
+        [field: SerializeField]
+        internal Rigidbody MyRigidbody {
+            get;
+            private set;
+        }
+
         [field: SerializeField]
         internal EventTrigger MyEventTrigger {
             get;
